6594428fcc094ca7cd30d5a03efb33f80b0e7ef3
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-04-12 18:38:56 -0300

    GUI building

--- gui.py
+++ gui.py
@@ -1,2 +1,9 @@
 import PySimpleGUI as sg
 
+sg.theme('BluePurple')
+
+layout = [[sg.Text('Escreva abaixo um numero entre 0 e 999:'),
+           sg.Text(size=(15, 1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Escrever'), sg.Button('Sair ')]]
+
